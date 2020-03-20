98aafbe13fd569f7e312adc5098da97127766fd4
Cloud User <ec2-user@ip-172-31-44-100.ap-south-1.compute.internal>
2020-03-20 07:15:38 +0000

    first commit

---
+++ 2.ListFiles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Author : Ram Shankar
+# Copyright (c) Ram Soft Tech
+# Script follows here:
+pwd
+ls
---
+++ 3.variables.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Author : Ram Shankar
+# Copyright (c) Ram Soft Tech
+# Script follows here:
+
+
+VAR_1="Hello world"
+_VAR_2=2020
+readonly NAME="Ram Shankar"
+
+echo $VAR1
+_VAR_2=2021
+
+unset _VAR_2
+echo $_VAR_2
+
+NAME="Ram"
+
